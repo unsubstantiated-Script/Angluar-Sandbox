3c4140894cfa5a51c8f0814e4bfcc4ee2146025d
Justin Fulton <jusfulton@gmail.com>
2020-12-12 07:33:59 -0800

    3.14 Using Types

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -13,29 +13,62 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
   //Properties
-  firstName = 'John';
-  lastName = 'Doe';
-  age = 30;
-  address = {
-    street: '50 Main Street',
-    city: 'Spokompton',
-    state: 'UA',
-    zip: '99007',
-  };
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: any;
+
+  foo: any;
+  hasKids: boolean;
+  numberArray: number[];
+  stringArray: string[];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unusable: void;
+  u: undefined;
+  n: null;
 
   // Methods
   constructor() {
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.age = 33;
+    this.address = {
+      street: '21st West Main',
+      city: 'Kowala',
+      state: 'WA',
+      zip: '99330',
+    };
+
+    this.foo = true;
+    this.hasKids = true;
+    this.numberArray = [1, 3, 5];
+    this.stringArray = ['hello', 'world'];
+    this.mixedArray = [1, 'grand', true, 'smelly'];
+    this.myTuple = ['helllo', 42, true];
+    this.unusable = undefined;
+    this.u = undefined;
+    this.n = null;
     //    this.sayHello()
-    console.log(this.age);
-    this.hasBirthday();
-    console.log(this.age);
+    // console.log(this.age);
+    // this.hasBirthday();
+    // console.log(this.age);
+    console.log(this.addNumbers(2, 3));
   }
 
-  sayHello() {
-    console.log(`Hello ${this.firstName}`);
+  showAge() {
+    return this.age + 2;
   }
 
-  hasBirthday() {
-    this.age += 1;
+  addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
   }
+
+  // sayHello() {
+  //   console.log(`Hello ${this.firstName}`);
+  // }
+
+  // hasBirthday() {
+  //   this.age += 1;
+  // }
 }
