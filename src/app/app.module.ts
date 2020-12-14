d95350e5e59f258d02452d5cc6d957eea13aad4f
Justin Fulton <jusfulton@gmail.com>
2020-12-14 11:38:18 -0800

    3.19 BootyStrap Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UsersComponent],
+  declarations: [AppComponent, UserComponent, UsersComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
